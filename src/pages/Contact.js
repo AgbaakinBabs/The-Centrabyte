cfb2b3daaff59e7f9f443d3457a63d849c1ed947
AgbaakinBabs <bdtitilola@gmail.com>
2025-05-16 11:13:14 +0100

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,13 +4,6 @@ export default function Contact() {
       <h2>Contact Us</h2>
       <p>Email: info@centrabyte.org</p>
       <p>Twitter: @thecentrabyte</p>
-<p>We value dialogue, collaboration, and the opportunity to hear from our readers. Whether you have questions about our content, ideas for collaboration, or just want to reach out with feedback, The Centrabyte is always open to meaningful connection.<p>
 
-<p>For inquiries related to legal commentary, guest contributions, compliance insights, or media-related discussions, please don’t hesitate to reach out. Our goal is to build a responsive and thoughtful community around the subjects we explore—and your voice is an important part of that mission.<p>
-
-<p>If you're interested in featuring your work, proposing a project, requesting a topic, or engaging us for commentary, we welcome your outreach. The Centrabyte is particularly open to collaborations that align with our focus areas: legal developments, corporate ethics, digital communication, workplace culture, and media innovation.<p>
-
-<p>Please allow up to 48 hours for a response, especially during newsletter release weeks. We look forward to hearing from you and hope to continue the conversation beyond the page.<p>
-    </div>
   );
 }
